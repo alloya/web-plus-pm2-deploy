ae70d9a4550713691b7f0343672bc070684fad2c
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2023-03-21 23:57:35 +0700

    pre-deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: '.env.deploy'});
+require('dotenv').config({ path: '.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF = 'origin/master',
@@ -18,7 +18,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `eval "$(ssh-agent -s)" && ssh-add -K /c/Users/Lenovo/.ssh/id_rsa && scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': "echo 'Deploying code to servers'",
       'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
