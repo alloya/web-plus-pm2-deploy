161c7e2a1f164d9ecd4d609d745a5b2e1dbb17c9
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2023-03-22 12:05:16 +0700

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,16 +13,15 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
-// app.use(cors({
-//   origin: [
-//     'http://alloya.nomoredomains.work',
-//     'https://alloya.nomoredomains.work',
-//     'http://localhost:3000',
-//   ],
-//   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-// }));
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://alloya.nomoredomains.work',
+    'https://alloya.nomoredomains.work',
+  ],
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
+  preflightContinue: false,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
